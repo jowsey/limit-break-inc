d2f0c88f1ffa230b570f6fc223067bd6c7a0cbe1
Thomas Jowsey <2302820@uad.ac.uk>
2025-09-27 08:06:08 +0100

    Add utils for formatting money & roman numerals

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -22,4 +22,39 @@ const formatWattHours = (wattHours: number) => {
 	return (wattHours * 1000).toPrecision(3) + ' mWh';
 };
 
-export { formatWatts, formatWattHours };
+const formatMoney = (money: number) => {
+	return '$' + money.toLocaleString(undefined, { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+};
+
+const numToRoman = (num: number) => {
+	if (num <= 0) return num.toString();
+
+	const romanNumerals: { [key: number]: string } = {
+		1000: 'M',
+		900: 'CM',
+		500: 'D',
+		400: 'CD',
+		100: 'C',
+		90: 'XC',
+		50: 'L',
+		40: 'XL',
+		10: 'X',
+		9: 'IX',
+		5: 'V',
+		4: 'IV',
+		1: 'I'
+	};
+
+	let result = '';
+	for (const value of Object.keys(romanNumerals)
+		.map(Number)
+		.sort((a, b) => b - a)) {
+		while (num >= value) {
+			result += romanNumerals[value];
+			num -= value;
+		}
+	}
+	return result;
+};
+
+export { formatWatts, formatWattHours, formatMoney, numToRoman };
