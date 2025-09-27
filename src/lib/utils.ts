a5860e4467a69e2faf00ab3cf39093201e223e94
Thomas Jowsey <2302820@uad.ac.uk>
2025-09-27 06:24:49 +0100

    Util for formatting watt-hours

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -10,4 +10,16 @@ const formatWatts = (watts: number) => {
 	return (watts * 1000).toPrecision(3) + ' mW';
 };
 
-export { formatWatts };
+const formatWattHours = (wattHours: number) => {
+	if (isNaN(wattHours) || !isFinite(wattHours)) return '???  Wh';
+	if (wattHours >= 1e18) return (wattHours / 1e18).toPrecision(3) + ' EWh';
+	if (wattHours >= 1e15) return (wattHours / 1e15).toPrecision(3) + ' PWh';
+	if (wattHours >= 1e12) return (wattHours / 1e12).toPrecision(3) + ' TWh';
+	if (wattHours >= 1e9) return (wattHours / 1e9).toPrecision(3) + ' GWh';
+	if (wattHours >= 1e6) return (wattHours / 1e6).toPrecision(3) + ' MWh';
+	if (wattHours >= 1e3) return (wattHours / 1e3).toPrecision(3) + ' kWh';
+	if (wattHours >= 1) return wattHours.toPrecision(3) + '  Wh';
+	return (wattHours * 1000).toPrecision(3) + ' mWh';
+};
+
+export { formatWatts, formatWattHours };
